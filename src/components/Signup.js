b62a3ef265c311c2d88d57ef9125fe9b3918333a
faisal5467 <faisalabbas3120@gmail.com>
2023-10-25 11:58:11 +0500

    signup field change

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -42,31 +42,7 @@ function Signup() {
 
         <div className="SignupContainer">
           <h1>Sign Up</h1>
-          {/* <div className="input-container">
-            <label>Username</label>
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="input-container">
-            <label>Password </label>
-            
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />          
-          </div>
-          <button className="SignupBut" onClick={handleSignup}>        
-            <Link to="/">
-              <p>Signup</p>
-            </Link>
-          </button>
-          <a href="/" style={{ alignSelf: "center" }}>
-            Have an account
-          </a> */}
+         
 
           {/* ///////////////// */}
           <form onSubmit={handleSubmit} >
@@ -122,16 +98,4 @@ function Signup() {
 }
 
 export default Signup;
-{
-  /* <form>
-        <div>
-          <label>Username:</label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button onClick={handleSignup}>Signup</button>
-      </form> */
-}
+
---
+++ src/components/extra.js
@@ -0,0 +1,39 @@
+ {/* <div className="input-container">
+            <label>Username</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <label>Password </label>
+            
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />          
+          </div>
+          <button className="SignupBut" onClick={handleSignup}>        
+            <Link to="/">
+              <p>Signup</p>
+            </Link>
+          </button>
+          <a href="/" style={{ alignSelf: "center" }}>
+            Have an account
+          </a> */}
+
+          {
+            /* <form>
+                  <div>
+                    <label>Username:</label>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                  </div>
+                  <div>
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                  </div>
+                  <button onClick={handleSignup}>Signup</button>
+                </form> */
+          }
